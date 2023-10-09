23e31463fb5f3e61b31e6dd8be2436a9f0b0b7b6
Derek Cavaliero <derek@derekcavaliero.com>
2023-10-09 12:35:07 -0700

    Fixes issues with data redaction in reportToUrl method. Changes most methods to private scope implements recursive merge for config in constructor.

--- src/DataLayerCop.js
+++ src/DataLayerCop.js
@@ -10,10 +10,10 @@
  * License: MIT
  */
 
-"use strict";
-
 class DataLayerCop 
 {
+
+  #config = {};
 
   constructor(config = {}) {
 
@@ -27,208 +27,57 @@ class DataLayerCop
       rules: [],
     };
 
-    this.config = {};
-
-    Object.assign(this.config, defaults, config);
+    this.#config = this.#recursiveMerge(defaults, config); 
       
-    this.setDataLayer();
+    this.#setDataLayer();
 
-    this.registerRules();
+    this.#registerRules();
 
-    if (!Array.isArray(this.config.rules) || !this.config.rules?.length) {
-      this.console('warn', 'No rules defined.');
+    if (!Array.isArray(this.#config.rules) || !this.#config.rules?.length) {
+      this.#reportToConsole('warn', 'No rules defined.');
       return;
     }
 
-    this.modifyPushMethod();
+    this.#modifyPushMethod();
 
-    this.getDataLayer().push({
+    this.#getDataLayer().push({
       event: 'datalayercop.loaded',
-      rules: this.config.rules
+      rules: this.#config.rules
     });
 
   }
 
-  setDataLayer() {
-    window[this.config.dataLayer] = window[this.config.dataLayer] || [];
-  }
-
-  getDataLayer() {
-    return window[this.config.dataLayer];
-  }
-
-  getPredefinedRule(key) {
-
-    const rules = {
-
-      event_property_exists: {
-        name: 'Expect payload to include an `event` property.',
-        assert: (payload) => payload.hasOwnProperty('event') && payload.event.length,
-        dropOnFail: false,
-        type: 'gtm',
-      },
-
-      event_is_namespaced: {
-        name: 'Expect `event` property value to be prefixed with a namespace.',
-        assert: (payload) => this.callStatic('isNamespaced', payload.event),
-        dropOnFail: false,
-        type: 'gtm',
-      },
-
-      payload_properties_are_preferred_case: {
-        name: `Expect all payload properties to match preferred case (${this.config.preferredCase}).`,
-        assert: (payload) => {
-          
-          const properties = Object.keys(payload);
-
-          for (let i = 0; i < properties.length; i++) {
-            if (!this.isPreferredCase(properties[i]))
-              return false;
-          }
-
-          return true;
-
-        },
-        dropOnFail: false
-      },
-
-      event_is_preferred_case_after_namespace: {
-        name: `Expect \`event\` property value to match preferred case (${this.config.preferredCase}) after namespace.`,
-        assert: (payload) => {
-
-          if (!this.callStatic('isNamespaced', payload.event))
-            return true;
-
-          const event = payload.event.split('.')[1];
-          return this.isPreferredCase(event);
-
-        },
-        dropOnFail: false,
-        type: 'gtm',
-      },
-
-    };
-
-    return rules[key] || {};
-
-  }
-
-  registerRules() {
-
-    this.config.rules = this.config.rules.map((rule) => {
-
-      if (rule.predefined) {
-        rule = Object.assign({}, this.getPredefinedRule(rule.predefined), rule);
-        delete rule.predefined;
-      }
-
-      return rule;
-
-    });
-
-  }
-
-  redactPayload(payload, payloadType) {
-
-    const pattern = 'name|email|tele|phone|address|street|country|city|state|province|region|zip|postal|country|birth|dob|born|gender|sex|race|ethnicity|height|weight|password';
-    
-    const properties = Object.keys(payload);
-
-    for (let i = 0; i < properties.length; i++) {
-    
-      let property = properties[i];
-      let value = payload[property];
-
-      if (typeof value === 'object') {
-        payload[property] = this.redactPayload(value);
-        continue;
-      }
+  #recursiveMerge(target, source) {
       
-      const regex = new RegExp(pattern, 'i');
-
-      if (regex.test(property))
-        payload[property] = '[REDACTED]';
-        
-    }
-    
-    return payload;
-
-  }
-
-  processRules(payload, payloadType) {
-
-    for (let i = 0; i < this.config.rules.length; i++) {
-
-      let rule = this.config.rules[i];
-
-      if (rule?.type && rule.type !== payloadType)
-        continue;
-
-      let test = this.enforce(rule, payload, payloadType);
-
-      if (test.passed)
-        continue;
-
-      if (this.config.report.only.includes(rule?.severity)) {
-        this.reportToUrl(test);
-        this.reportToDataLayer(test);
-      }
-      
-      if (rule.dropOnFail)
-        return false;
-      
-    }
-    
-    return payload;
-
-  }
-
-  reportToDataLayer(test) {
-
-    if (this.config.report.toDataLayer === false)
-      return;
-
-    test.payload = JSON.stringify(test.payload);
-
-    this.getDataLayer().push({
-      'event': 'gtm.pageError',
-      'gtm.errorMessage': test.rule?.name,
-      'datalayercop': test,
-    });
-
-  }
-
-  reportToUrl(test) {
-
-    if (!this.isValidUrl(this.config.report.toUrl)) {
-      this.console('warn', `Attempted to report to URL - but an invalid URL was provided (${this.config.report.toUrl}).`);
-      return;
+    for (const key of Object.keys(source)) {
+      if (source[key] instanceof Object)
+        Object.assign(source[key], this.#recursiveMerge(target[key], source[key]));
     }
 
-    let payload = {
-      hostname: location.hostname,
-      url: location.href,
-      user_agent: navigator.userAgent,
-    };
+    Object.assign(target || {}, source);
 
-    test.payload = this.redactPayload(test.payload);
-
-    Object.assign(payload, test);
-
-    navigator.sendBeacon(this.config.report.toUrl, JSON.stringify(payload));
-
+    return target;
+  
   }
 
-  modifyPushMethod() {
+  #setDataLayer() {
+    window[this.#config.dataLayer] = window[this.#config.dataLayer] || [];
+  }
+
+  #getDataLayer() {
+    return window[this.#config.dataLayer];
+  }
+
+  #modifyPushMethod() {
 
     var _this = this;
-    var originalPush = this.getDataLayer().push;
+    var originalPush = this.#getDataLayer().push;
     
-    this.getDataLayer().push = function() {
+    this.#getDataLayer().push = function() {
 
       let payload;
 
-      if (_this.isArgumentsObject(arguments[0])) {
+      if (_this.#isArgumentsObject(arguments[0])) {
           
         /**
          * If arguments[0] is an arguments object, it is more than likely a gtag command.
@@ -239,9 +88,9 @@ class DataLayerCop
         let command = arguments[0];
         
         if (command[0] == 'event')
-          payload = _this.processRules(command, 'gtag');
+          payload = _this.#processRules(command, 'gtag');
 
-      } else if (_this.callStatic('isObjectLiteral', arguments[0])) {
+      } else if (_this.#callStatic('isObjectLiteral', arguments[0])) {
 
         /**
          * If argument[0] is an object literal - its likely a Google Tag Manager dataLayer payload. 
@@ -251,7 +100,7 @@ class DataLayerCop
 
         // All dataLayer events namespaced with 'gtm.' are reserved for GTM internal use and should bypass any rules.
         if (!payload?.event?.startsWith('gtm.'))
-          payload = _this.processRules(payload, 'gtm');
+          payload = _this.#processRules(payload, 'gtm');
 
       }
 
@@ -265,7 +114,78 @@ class DataLayerCop
 
   }
 
-  enforce(rule = {}, payload, payloadType) {
+  #registerRules() {
+
+    this.#config.rules = this.#config.rules.map((rule) => {
+
+      if (rule.predefined) {
+        rule = Object.assign({}, this.#getPredefinedRule(rule.predefined), rule);
+        delete rule.predefined;
+      }
+
+      return rule;
+
+    });
+
+  }
+
+  #getPredefinedRule(key) {
+
+    const rules = {
+
+      event_property_exists: {
+        name: 'Expect payload to include an `event` property.',
+        assert: (payload) => payload.hasOwnProperty('event') && payload.event.length,
+        dropOnFail: false,
+        type: 'gtm',
+      },
+
+      event_is_namespaced: {
+        name: 'Expect `event` property value to be prefixed with a namespace.',
+        assert: (payload) => this.#callStatic('isNamespaced', payload.event),
+        dropOnFail: false,
+        type: 'gtm',
+      },
+
+      payload_properties_are_preferred_case: {
+        name: `Expect all payload properties to match preferred case (${this.#config.preferredCase}).`,
+        assert: (payload) => {
+          
+          const properties = Object.keys(payload);
+
+          for (let i = 0; i < properties.length; i++) {
+            if (!this.#isPreferredCase(properties[i]))
+              return false;
+          }
+
+          return true;
+
+        },
+        dropOnFail: false
+      },
+
+      event_is_preferred_case_after_namespace: {
+        name: `Expect \`event\` property value to match preferred case (${this.#config.preferredCase}) after namespace.`,
+        assert: (payload) => {
+
+          if (!this.#callStatic('isNamespaced', payload.event))
+            return true;
+
+          const event = payload.event.split('.')[1];
+          return this.#isPreferredCase(event);
+
+        },
+        dropOnFail: false,
+        type: 'gtm',
+      },
+
+    };
+
+    return rules[key] || {};
+
+  }
+
+  #enforceRule(rule = {}, payload, payloadType) {
 
     let test = {
       rule,
@@ -275,7 +195,7 @@ class DataLayerCop
 
     if (typeof rule.assert !== 'function') {
       
-      this.console('warn', 'Rule object is missing assert method - skipping...', rule); 
+      this.#reportToConsole('warn', 'Rule object is missing assert method - skipping...', rule); 
       
       test.passed = true;
       return test;
@@ -285,25 +205,117 @@ class DataLayerCop
     test.passed = rule.assert(payload, payloadType);
     
     if (!test.passed)
-      this.console('warn', `${payloadType} payload failed rule: ${rule?.name}`, test);
+      this.#reportToConsole('warn', `${payloadType} payload failed rule: ${rule?.name}`, test);
 
     return test;
 
   }
 
-  callStatic(method, ...args) {
-    return DataLayerCop[method].apply(this, args);
+  #processRules(payload, payloadType) {
+
+    for (let i = 0; i < this.#config.rules.length; i++) {
+
+      let rule = this.#config.rules[i];
+
+      if (rule?.type && rule.type !== payloadType)
+        continue;
+
+      let test = this.#enforceRule(rule, payload, payloadType);
+
+      if (test.passed)
+        continue;
+
+      if (this.#config.report.only.includes(rule?.severity)) {
+
+        this.#reportToDataLayer(test);
+
+        if (this.#config.report.toUrl)
+          this.#reportToUrl(test);
+
+      }
+      
+      if (rule.dropOnFail)
+        return false;
+      
+    }
+    
+    return payload;
+
   }
 
-  console(method, message, data) {
-    console[method](`🚨 ${this.config.dataLayer} Cop - ${message}`, data);
+
+  #reportToConsole(method, message, data) {
+    console[method](`🚨 ${this.#config.dataLayer} Cop - ${message}`, data);
   }
 
-  isArgumentsObject(item) {
+  #reportToDataLayer(test) {
+
+    if (this.#config.report.toDataLayer === false)
+      return;
+
+    this.#getDataLayer().push({
+      'event': 'gtm.pageError',
+      'gtm.errorMessage': test.rule?.name,
+      'datalayercop': test,
+    });
+
+  }
+
+  #reportToUrl(rule) {
+
+    if (!this.#isValidUrl(this.#config.report.toUrl)) {
+      this.#reportToConsole('warn', `Attempted to report to URL - but an invalid URL was provided (${this.#config.report.toUrl}).`, payload);
+      return;
+    }
+
+    let data = {
+      hostname: location.hostname,
+      url: location.href,
+      user_agent: navigator.userAgent,
+    };
+
+    Object.assign(data, rule);
+
+    data.payload = this.#redactPayload(data.payload);
+
+    navigator.sendBeacon(this.#config.report.toUrl, JSON.stringify(data));
+
+  }
+
+  #redactPayload(payload, payloadType) {
+
+    const pattern = 'name|email|tele|phone|address|street|country|city|state|province|region|zip|postal|country|birth|dob|born|gender|sex|race|ethnicity|height|weight|password';
+    
+    let redactedPayload = JSON.parse(JSON.stringify(payload));
+
+    const properties = Object.keys(redactedPayload);
+
+    for (let i = 0; i < properties.length; i++) {
+    
+      let property = properties[i];
+      let value = redactedPayload[property];
+
+      if (typeof value === 'object') {
+        redactedPayload[property] = this.#redactPayload(value);
+        continue;
+      }
+      
+      const regex = new RegExp(pattern, 'i');
+
+      if (regex.test(property))
+        redactedPayload[property] = '(redacted)';
+        
+    }
+    
+    return redactedPayload;
+
+  }
+
+  #isArgumentsObject(item) {
     return Object.prototype.toString.call(item) === '[object Arguments]';
   }
 
-  isValidUrl(string) {
+  #isValidUrl(string) {
     
     try {
       var url = new URL(string);
@@ -313,6 +325,14 @@ class DataLayerCop
 
     return url.protocol === 'https:';
 
+  }
+
+  #isPreferredCase(string) {
+    return DataLayerCop.getCommonPattern(this.#config.preferredCase).test(string);
+  }
+
+  #callStatic(method, ...args) {
+    return DataLayerCop[method].apply(this, args);
   }
 
   static getCommonPattern(pattern) {
@@ -330,10 +350,6 @@ class DataLayerCop
         return pattern;
     }
 
-  }
-
-  isPreferredCase(string) {
-    return DataLayerCop.getCommonPattern(this.config.preferredCase).test(string);
   }
 
   static isObjectLiteral(input) {
